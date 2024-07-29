93529f5d1d1b597f2f1bb8cae935506665a0b0f0
Dani Ramadhan <164002193+Dani170907@users.noreply.github.com>
2024-07-29 09:27:30 +0700

    Update proses.php

--- Martabak/proses.php
+++ Martabak/proses.php
@@ -1,24 +1,17 @@
 <?php
-// Memulai session
 session_start();
 
-// Mengecek apakah session sudah ada isinya atau belum
-
-// Jika ada session dengan nama 'tmpMartabak' maka tampung dalam variabel 'tmpNamaMartabak'
 if (isset($_SESSION['tmpNamaMartabak'])) {
     $tmpNamaMartabak = $_SESSION['tmpNamaMartabak'];
 }
-
 if (isset($_SESSION['tmpVarianMartabak'])) {
     $tmpVarianMartabak = $_SESSION['tmpVarianMartabak'];
 }
-
 if (isset($_SESSION['tmpPorsiMartabak'])) {
     $tmpPorsiMartabak = $_SESSION['tmpPorsiMartabak'];
 }
-
-if (isset($_SESSION['tmpHargaMartabak'])) {
-    $tmpHargaMartabak = $_SESSION['tmpHargaMartabak'];
+if (isset($_SESSION['hargaMartabak'])) {
+    $hargaMartabak = $_SESSION['hargaMartabak'];
 }
 
 $namaMartabak = $_GET['namaMartabak'];
@@ -31,9 +24,7 @@ $_SESSION['varianMartabak'] = $varianMartabak;
 $_SESSION['porsiMartabak'] = $porsiMartabak;
 $_SESSION['hargaMartabak'] = $hargaMartabak;
 
-echo "Nama " . $_SESSION['namaMartabak'] . "<br>";
-echo "Varian " . $_SESSION['varianMartabak'] . "<br>";
-echo "Porsi " . $_SESSION['porsiMartabak'] . "<br>";
-echo "Harga " . $_SESSION['hargaMartabak'] . "<br>";
-
-?>
+echo "Nama : " . $_SESSION['namaMartabak'] . "<br>";
+echo "Varian : " . $_SESSION['varianMartabak'] . "<br>";
+echo "Porsi : " . $_SESSION['porsiMartabak'] . "<br>";
+echo "Harga : " . $_SESSION['hargaMartabak'] . "<br>";
